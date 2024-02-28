bd87ec64c783f908adfda461d998fe28fcf0685b
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-02-28 11:33:53 +0800

    add copy keymap

--- lua/paooap/remap.lua
+++ lua/paooap/remap.lua
@@ -1,2 +1,8 @@
 
 vim.g.mapleader = " "
+
+
+-- this commands use to copy the selected text to the system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
