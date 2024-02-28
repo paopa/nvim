a85776d4324cd5756d7e5ef05829817701626e81
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-02-28 11:42:47 +0800

    add paste command in keymap

--- lua/paooap/remap.lua
+++ lua/paooap/remap.lua
@@ -2,7 +2,11 @@
 vim.g.mapleader = " "
 
 
+-- it will delete the selected text and paste the content in the default register to the current position
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
 -- this commands use to copy the selected text to the system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
 
