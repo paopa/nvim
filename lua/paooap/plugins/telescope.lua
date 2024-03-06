c930d05d0acb76f1371b03c94a0831b3e2e90fd2
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-07 02:11:43 +0800

    add telescope to easy find file, buffer, and so on..

---
+++ lua/paooap/plugins/telescope.lua
@@ -0,0 +1,31 @@
+return {
+    "nvim-telescope/telescope.nvim",
+
+    tag = "0.1.5",
+
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+
+    config = function()
+        require('telescope').setup({})
+
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>fw', function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>fW', function()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>gs', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+        vim.keymap.set('n', '<leader>ht', builtin.help_tags, {})    
+    end
+}
+
