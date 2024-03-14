a1344196ddf95ebfae56ead7132ffad483855a19
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-15 00:08:00 +0800

    refactor the structure of the config

--- init.lua
+++ init.lua
@@ -1,1 +1,26 @@
-require("paooap")
+-- Boostrap the lazy.nvim plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- These modules are loaded by default
+require("core.options")
+require("core.keymaps")
+
+-- Initialize lazy with dynamic laoding of plugins
+require("lazy").setup("plugins", {
+    change_detection = {
+        enable = true, -- Detect changes in the plugins directory
+        notify = false, -- Do not notify on changes
+    }
+})
+
--- lua/paooap/init.lua
+++
@@ -1,3 +0,0 @@
-require("paooap.set")
-require("paooap.remap")
-require("paooap.lazy")
