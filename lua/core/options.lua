23e303199a95827568ffc3d2ef70729419955746
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-15 00:48:44 +0800

    update nvim options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,34 +1,76 @@
-vim.opt.guicursor = ""
+local opt = vim.opt
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+opt.guicursor = ""
 
-vim.opt.tabstop = 8
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+-- Line Number
+opt.number = true
+opt.relativenumber = true
 
-vim.opt.smartindent = true -- not sure the option, but keep it true
+-- Tabs and Indentation
+opt.tabstop = 8 -- Number of spaces that a <Tab> in the file counts for
+opt.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+-- Use spaces instead of tabs, so when you hit <Tab> it inserts spaces instead of a tab character
+-- To insert a real tab when 'expandtab' is on, use CTRL-V<Tab>.
+opt.expandtab = true 
+opt.autoindent = true -- Copy indent from current line when starting a new line
+-- Number of spaces that a <Tab> counts for while performing editing operations, like inserting a <Tab> or using <BS>
+opt.softtabstop = 4 
 
-vim.opt.wrap = false
+-- Line Wrapping
+opt.wrap = false
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+-- Search Settings
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
+opt.incsearch = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+-- Cursor Line
+opt.cursorline = true 
+opt.scrolloff = 8 -- Minimal number of screen lines to keep above and below the cursor
 
-vim.opt.termguicolors = true
+-- Appearance
+opt.termguicolors = true -- Enable 24-bit RGB color in the TUI
+opt.background = "dark" -- Set the default background to dark
+opt.signcolumn = "yes" -- Always show the sign column, otherwise it would shift the text each time
+opt.colorcolumn = "120" -- Highlight the 120th column
+vim.diagnostic.config({
+    float = {
+        border = "rounded", -- add rounded border to the diagnostics
+    }
+})
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@") -- not pretty sure, but feel the sense, I think.
+-- Backspace
+opt.backspace = "indent,eol,start" -- Allow backspacing over everything in insert mode
 
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "120"
-
+-- Clipboard
 vim.api.nvim_set_option("clipboard","unnamed")
+
+-- Split Window
+opt.splitright = true
+opt.splitbelow = true
+
+-- Disable Swap and Backup
+opt.swapfile = false 
+opt.backup = false 
+
+-- Undodir 
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir" 
+opt.undofile = true 
+
+-- Listchars
+opt.isfname:append("@-@") -- Add @ to the list of characters that are allowed in a file name
+opt.iskeyword:append("-") -- Add - to the list of characters that are allowed in a word
+
+-- CursorHold
+opt.updatetime = 50 -- I disable swapfile, so it only used for CursorHold autocommand events
+
+-- Mouse
+opt.mouse = "" -- Disable mouse support
+
+-- Folding
+opt.foldlevel = 20 -- Maximum fold depth when starting editing
+opt.foldmethod = "expr" -- Use expression to determine folds
+opt.foldexpr = "nvim_treesitter#foldexpr()" -- Use treesitter to determine folds
+
 
