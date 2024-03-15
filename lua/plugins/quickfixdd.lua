7449a8b9f49d37bc32b4b49c66b93b561affffd6
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-16 04:00:38 +0800

    update the keymap for quickfixdd

--- lua/plugins/quickfixdd.lua
+++ lua/plugins/quickfixdd.lua
@@ -3,4 +3,14 @@ return {
   -- https://github.com/TamaMcGlinn/quickfixdd
   'TamaMcGlinn/quickfixdd',
   event = 'VeryLazy',
+  config = function()
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>qo", ":copen<CR>") -- open quickfix list
+    keymap.set("n", "<leader>qf", ":cfirst<CR>") -- jump to first quickfix list item
+    keymap.set("n", "<leader>qn", ":cnext<CR>") -- jump to next quickfix list item
+    keymap.set("n", "<leader>qp", ":cprev<CR>") -- jump to prev quickfix list item
+    keymap.set("n", "<leader>ql", ":clast<CR>") -- jump to last quickfix list item
+    keymap.set("n", "<leader>qc", ":cclose<CR>") -- close quickfix list
+  end,
 }
+-- I'm not sure the use case for this plugin. but I'm going to keep it a while to see if it's useful.
