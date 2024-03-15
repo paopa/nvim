4150559320131b1ad702d8beb446c30b8fdee4d5
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-15 23:15:06 +0800

    comment the basic keymaps in the nvim-tree config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -27,3 +27,59 @@ return {
     keymap.set("n", "<leader>ef", ":NvimTreeFindFile<CR>") -- find file in file explorer
   end
 }
+
+-- https://github.com/nvim-tree/nvim-tree.lua/blob/master/doc/nvim-tree-lua.txt
+--
+-- There are the Basic keymaps I think are useful:
+-- 
+-- `<C-]>`           CD                        
+-- `<C-e>`           Open: In Place            
+-- `<C-k>`           Info                      
+-- `<C-r>`           Rename: Omit Filename     
+-- `<C-t>`           Open: New Tab             
+-- `<C-v>`           Open: Vertical Split      
+-- `<C-x>`           Open: Horizontal Split    
+-- `<BS>`            Close Directory           
+-- `<CR>`            Open                      
+-- `<Tab>`           Open Preview              
+-- `>`               Next Sibling              
+-- `<`               Previous Sibling          
+-- `.`               Run Command               
+-- `-`               Up                        
+-- `a`               Create File Or Directory  
+-- `B`               Toggle Filter: No Buffer 
+-- `c`               Copy                     
+-- `C`               Toggle Filter: Git Clean 
+-- `[c`              Prev Git                
+-- `]c`              Next Git                
+-- `d`               Delete                  
+-- `D`               Trash                   
+-- `E`               Expand All              
+-- `e`               Rename: Basename        
+-- `]e`              Next Diagnostic         
+-- `[e`              Prev Diagnostic         
+-- `F`               Live Filter: Clear        
+-- `f`               Live Filter: Start       
+-- `g?`              Help                     
+-- `gy`              Copy Absolute Path       
+-- `ge`              Copy Basename            
+-- `H`               Toggle Filter: Dotfiles  
+-- `I`               Toggle Filter: Git Ignore
+-- `J`               Last Sibling             
+-- `K`               First Sibling            
+-- `L`               Toggle Group Empty       
+-- `o`               Open                      
+-- `O`               Open: No Window Picker    
+-- `p`               Paste                     
+-- `P`               Parent Directory          
+-- `q`               Close                     
+-- `r`               Rename                    
+-- `R`               Refresh                   
+-- `s`               Run System                
+-- `S`               Search                    
+-- `u`               Rename: Full Path         
+-- `U`               Toggle Filter: Hidden     
+-- `W`               Collapse                  
+-- `x`               Cut                       
+-- `y`               Copy Name                 
+-- `Y`               Copy Relative Path        
