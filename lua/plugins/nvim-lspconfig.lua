58e1555fb33fca5f9aa8e259ace655aa5988f115
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-15 14:01:11 +0800

    add nvim-lspconfig plugin

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,98 @@
+-- LSP Support
+return {
+  -- LSP Configuration
+  -- https://github.com/neovim/nvim-lspconfig
+  'neovim/nvim-lspconfig',
+  event = 'VeryLazy',
+  dependencies = {
+    -- LSP Management
+    -- https://github.com/williamboman/mason.nvim
+    { 'williamboman/mason.nvim' },
+    -- https://github.com/williamboman/mason-lspconfig.nvim
+    { 'williamboman/mason-lspconfig.nvim' },
+
+    -- Useful status updates for LSP
+    -- https://github.com/j-hui/fidget.nvim
+    { 'j-hui/fidget.nvim', opts = {} },
+
+    -- Additional lua configuration, makes nvim stuff amazing!
+    -- https://github.com/folke/neodev.nvim
+    {'folke/neodev.nvim' },
+  },
+  config = function ()
+    require('mason').setup()
+    require('mason-lspconfig').setup({
+      -- Install these LSPs automatically
+      ensure_installed = {
+        -- 'bashls', -- requires npm to be installed
+        -- 'cssls', -- requires npm to be installed
+        -- 'html', -- requires npm to be installed
+        'lua_ls',
+        -- 'jsonls', -- requires npm to be installed
+        'lemminx',
+        'marksman',
+        'quick_lint_js',
+        -- 'tsserver', -- requires npm to be installed
+        -- 'yamlls', -- requires npm to be installed
+      }
+    })
+
+    local lspconfig = require('lspconfig')
+    local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local lsp_attach = function(client, bufnr)
+      -- Create your keybindings here...
+    end
+
+    -- Call setup on each LSP server
+    require('mason-lspconfig').setup_handlers({
+      function(server_name)
+        lspconfig[server_name].setup({
+          on_attach = lsp_attach,
+          capabilities = lsp_capabilities,
+        })
+      end
+    })
+
+    -- Lua LSP settings
+    lspconfig.lua_ls.setup {
+      settings = {
+        Lua = {
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {'vim'},
+          },
+        },
+      },
+    }
+
+    -- Globally configure all LSP floating preview popups (like hover, signature help, etc)
+    local open_floating_preview = vim.lsp.util.open_floating_preview
+    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+      opts = opts or {}
+      opts.border = opts.border or "rounded" -- Set border to rounded
+      return open_floating_preview(contents, syntax, opts, ...)
+    end
+
+
+    -- Keymaps for LSP
+    local keymap = vim.keymap
+    keymap.set('n', '<leader>gg', '<cmd>lua vim.lsp.buf.hover()<CR>')
+    keymap.set('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+    keymap.set('n', '<leader>gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+    keymap.set('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+    keymap.set('n', '<leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+    keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+    keymap.set('n', '<leader>gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+    keymap.set('n', '<leader>rr', '<cmd>lua vim.lsp.buf.rename()<CR>')
+    keymap.set('n', '<leader>gf', '<cmd>lua vim.lsp.buf.format({async = true})<CR>')
+    keymap.set('v', '<leader>gf', '<cmd>lua vim.lsp.buf.format({async = true})<CR>')
+    keymap.set('n', '<leader>ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+    keymap.set('n', '<leader>gl', '<cmd>lua vim.diagnostic.open_float()<CR>')
+    keymap.set('n', '<leader>gp', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+    keymap.set('n', '<leader>gn', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+    keymap.set('n', '<leader>tr', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+    keymap.set('i', '<C-Space>', '<cmd>lua vim.lsp.buf.completion()<CR>')
+
+  end
+}
+
