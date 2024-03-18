f5a1e7f8ae9514474853f59e30e1a657ad18a54c
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-19 02:10:22 +0800

    add file type config for python and markdown

---
+++ ftplugin/python.lua
@@ -0,0 +1,21 @@
+-- Filetype-specific keymaps (these can be done in the ftplugin directory instead if you prefer)
+--
+local keymap = vim.keymap
+
+keymap.set("n", '<leader>go', function()
+  if vim.bo.filetype == 'python' then
+    vim.api.nvim_command('PyrightOrganizeImports')
+  end
+end)
+
+keymap.set("n", '<leader>tc', function()
+  if vim.bo.filetype == 'python' then
+    require('dap-python').test_class();
+  end
+end)
+
+keymap.set("n", '<leader>tm', function()
+  if vim.bo.filetype == 'python' then
+    require('dap-python').test_method();
+  end
+end)
