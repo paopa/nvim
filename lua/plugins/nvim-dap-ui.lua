64bcaf9209ece57f018f42290e63d5575a468665
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-19 01:51:04 +0800

    add dependency for nvim-dap-ui

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -7,9 +7,11 @@ return {
     -- https://github.com/mfussenegger/nvim-dap
     'mfussenegger/nvim-dap',
     -- https://github.com/theHamsta/nvim-dap-virtual-text
-    'theHamsta/nvim-dap-virtual-text', -- inline variable text while debugging
+    'theHamsta/nvim-dap-virtual-text',   -- inline variable text while debugging
     -- https://github.com/nvim-telescope/telescope-dap.nvim
     'nvim-telescope/telescope-dap.nvim', -- telescope integration with dap
+    -- https://github.com/nvim-neotest/nvim-nio
+    'nvim-neotest/nvim-nio', -- async IO lib in neovim
   },
   opts = {
     controls = {
@@ -86,7 +88,7 @@ return {
       max_value_lines = 100
     }
   },
-  config = function (_, opts)
+  config = function(_, opts)
     local dap = require('dap')
     require('dapui').setup(opts)
 
@@ -114,21 +116,29 @@ return {
     local keymap = vim.keymap
     keymap.set("n", "<leader>bb", "<cmd>lua require'dap'.toggle_breakpoint()<cr>")
     keymap.set("n", "<leader>bc", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>")
-    keymap.set("n", "<leader>bl", "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>")
+    keymap.set("n", "<leader>bl",
+      "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>")
     keymap.set("n", '<leader>br', "<cmd>lua require'dap'.clear_breakpoints()<cr>")
     keymap.set("n", '<leader>ba', '<cmd>Telescope dap list_breakpoints<cr>')
     keymap.set("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>")
     keymap.set("n", "<leader>dj", "<cmd>lua require'dap'.step_over()<cr>")
     keymap.set("n", "<leader>dk", "<cmd>lua require'dap'.step_into()<cr>")
     keymap.set("n", "<leader>do", "<cmd>lua require'dap'.step_out()<cr>")
-    keymap.set("n", '<leader>dd', function() require('dap').disconnect(); require('dapui').close(); end)
-    keymap.set("n", '<leader>dt', function() require('dap').terminate(); require('dapui').close(); end)
+    keymap.set("n", '<leader>dd', function()
+      require('dap').disconnect(); require('dapui').close();
+    end)
+    keymap.set("n", '<leader>dt', function()
+      require('dap').terminate(); require('dapui').close();
+    end)
     keymap.set("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>")
     keymap.set("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>")
     keymap.set("n", '<leader>di', function() require "dap.ui.widgets".hover() end)
-    keymap.set("n", '<leader>d?', function() local widgets = require "dap.ui.widgets"; widgets.centered_float(widgets.scopes) end)
+    keymap.set("n", '<leader>d?',
+      function()
+        local widgets = require "dap.ui.widgets"; widgets.centered_float(widgets.scopes)
+      end)
     keymap.set("n", '<leader>df', '<cmd>Telescope dap frames<cr>')
     keymap.set("n", '<leader>dh', '<cmd>Telescope dap commands<cr>')
-    keymap.set("n", '<leader>de', function() require('telescope.builtin').diagnostics({default_text=":E:"}) end)
+    keymap.set("n", '<leader>de', function() require('telescope.builtin').diagnostics({ default_text = ":E:" }) end)
   end
 }
