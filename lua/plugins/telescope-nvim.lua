fe197c4783418a5acd4d426d50d506307562140e
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-21 02:14:33 +0800

    refactor the telescope-nvim config

--- lua/plugins/telescope-nvim.lua
+++ lua/plugins/telescope-nvim.lua
@@ -29,39 +29,83 @@ return {
     }
   },
 
-  config = function()
-    require('telescope').setup({})
-
-    local builtin = require('telescope.builtin')
-    local keymap = vim.keymap
-
-    keymap.set('n', '<leader>ff', function()
-      -- builtin.find_files({ hidden = true, no_ignore = true }) -- I'm still considering whether to use these options
-      builtin.find_files()
-    end)
-    keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    keymap.set('n', '<leader>fb', builtin.buffers, {})
-    keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    keymap.set('n', '<leader>fs', builtin.current_buffer_fuzzy_find, {})
-    keymap.set('n', '<leader>fo', builtin.lsp_document_symbols, {})
-    keymap.set('n', '<leader>fi', builtin.lsp_incoming_calls, {})
-    keymap.set('n', '<leader>fm', function()
-      builtin.treesitter({ default_text = ":method:" })
-    end)
-
-    -- what is the difference between <leader>fw and <leader>fW?
-
-    -- In Vim, a "word" is a sequence of letters, digits and underscores,
-    -- or a sequence of other non-blank characters, separated with white space.
-    keymap.set('n', '<leader>fw', function()
-      local word = vim.fn.expand("<cword>")
-      builtin.grep_string({ search = word })
-    end)
-    -- In Vim, a "WORD" is a sequence of non-blank characters, separated with white space.
-    -- This means it includes punctuation and other special characters as part of the WORD.
-    keymap.set('n', '<leader>fW', function()
-      local word = vim.fn.expand("<cWORD>")
-      builtin.grep_string({ search = word })
-    end)
-  end
+  keys = {
+    {
+      '<leader>ff',
+      function()
+        -- builtin.find_files({ hidden = true, no_ignore = true }) -- I'm still considering whether to use these options
+        require('telescope.builtin').find_files()
+      end,
+      desc = 'find files'
+    },
+    {
+      '<leader>fg',
+      function()
+        require('telescope.builtin').live_grep()
+      end,
+      desc = 'live grep'
+    },
+    {
+      '<leader>fb',
+      function()
+        require('telescope.builtin').buffers()
+      end,
+      desc = 'find buffers'
+    },
+    {
+      '<leader>fh',
+      function()
+        require('telescope.builtin').help_tags()
+      end,
+      desc = 'help tags'
+    },
+    {
+      '<leader>fs',
+      function()
+        require('telescope.builtin').current_buffer_fuzzy_find()
+      end,
+      desc = 'current buffer fuzzy find'
+    },
+    {
+      '<leader>fo',
+      function()
+        require('telescope.builtin').lsp_document_symbols()
+      end,
+      desc = 'lsp document symbols'
+    },
+    {
+      '<leader>fi',
+      function()
+        require('telescope.builtin').lsp_incoming_calls()
+      end,
+      desc = 'lsp incoming calls'
+    },
+    {
+      '<leader>fm',
+      function()
+        require('telescope.builtin').treesitter({ default_text = ":method:" })
+      end,
+      desc = 'treesitter'
+    },
+    {
+      '<leader>fw',
+      function()
+        local word = vim.fn.expand("<cword>")
+        require('telescope.builtin').grep_string({ search = word })
+      end,
+      desc = 'grep word'
+      -- In Vim, a "word" is a sequence of letters, digits and underscores,
+      -- or a sequence of other non-blank characters, separated with white space.
+    },
+    {
+      '<leader>fW',
+      function()
+        local word = vim.fn.expand("<cWORD>")
+        require('telescope.builtin').grep_string({ search = word })
+      end,
+      desc = 'grep WORD'
+      -- In Vim, a "WORD" is a sequence of non-blank characters, separated with white space.
+      -- This means it includes punctuation and other special characters as part of the WORD.
+    },
+  },
 }
