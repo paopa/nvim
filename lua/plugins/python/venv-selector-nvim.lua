28edf13dcc73ab686212cb353704daac53f68431
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-22 21:57:16 +0800

    automate load the cached venv

--- lua/plugins/python/venv-selector-nvim.lua
+++ lua/plugins/python/venv-selector-nvim.lua
@@ -4,7 +4,7 @@ return {
   fp = 'python',
   dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
   opts = {
-    name = {".venv"},
+    name = { ".venv" },
     -- auto_refresh = false
   },
   event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
@@ -14,4 +14,23 @@ return {
     -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
     { '<leader>pvc', '<cmd>VenvSelectCached<cr>' },
   },
+  config = function(_, opts)
+    require('venv-selector').setup(opts)
+    vim.api.nvim_create_autocmd(
+    -- the plugin docs uses VimEnter but I prefer BufReadPre
+      { 'BufReadPre' },
+      {
+        desc = 'Auto select virtualenv Nvim open',
+        pattern = '*',
+        callback = function()
+          local venv = vim.fn.findfile('pyproject.toml', vim.fn.getcwd() .. ';')
+          if venv ~= '' then
+            require('venv-selector').retrieve_from_cache()
+          end
+        end,
+        once = true,
+      })
+  end
 }
+-- I used this plugin to manage the virtualenvs to control which python interpreter to use for a project.
+-- and using code runner plugin to run the code with the selected interpreter.
