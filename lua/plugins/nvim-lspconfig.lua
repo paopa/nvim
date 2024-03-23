d18679508b822abf4dd786a1949ecbde21431662
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-23 13:32:54 +0800

    update the keymap for lsp config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -24,9 +24,17 @@ return {
     local opts = { noremap = true, silent = true }
 
     -- Global mappings
-    opts.desc = "Format Code"
-    keymap.set("n", "<leader>fc", "<cmd>lua vim.lsp.buf.format({async = true})<CR>", opts)
-    keymap.set("v", "<leader>fc", "<cmd>lua vim.lsp.buf.format({async = true})<CR>", opts)
+    opts.desc = "Show buffer diagnostics"
+    keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+    opts.desc = "Show line diagnostics"
+    keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+    opts.desc = "Go to previous diagnostic"
+    keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+    opts.desc = "Go to next diagnostic"
+    keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
     local on_attach = function(client, bufnr)
       opts.buffer = bufnr
@@ -58,18 +66,6 @@ return {
 
       opts.desc = "Smart rename"
       keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-      opts.desc = "Show buffer diagnostics"
-      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-      opts.desc = "Show line diagnostics"
-      keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-      opts.desc = "Go to previous diagnostic"
-      keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-      opts.desc = "Go to next diagnostic"
-      keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
       opts.desc = "Show documentation for what is under cursor"
       keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
