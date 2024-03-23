af4e7b28ac5ccb6ce6ff13b2f5b72034cce7e58e
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-23 15:16:13 +0800

    use ruff-lsp and pyright as the linter, formatter, and python lsp server

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,51 +1,53 @@
 return {
-	-- LSP Management
-	-- https://github.com/williamboman/mason.nvim
-	"williamboman/mason.nvim",
-	dependencies = {
-		-- https://github.com/williamboman/mason-lspconfig.nvim
-		{ "williamboman/mason-lspconfig.nvim" },
-		-- Install or upgrade all of the third-part tools.
-		-- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
-		{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
-	},
-	config = function()
-		require("mason").setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+  -- LSP Management
+  -- https://github.com/williamboman/mason.nvim
+  "williamboman/mason.nvim",
+  dependencies = {
+    -- https://github.com/williamboman/mason-lspconfig.nvim
+    { "williamboman/mason-lspconfig.nvim" },
+    -- Install or upgrade all of the third-part tools.
+    -- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
+    { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+  },
+  config = function()
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
 
-		require("mason-lspconfig").setup({
-			-- Install these LSPs automatically
-			ensure_installed = {
-				"bashls", -- requires npm to be installed
-				"lua_ls",
-				"jsonls", -- requires npm to be installed
-				"lemminx",
-				"marksman",
-				"yamlls", -- requires npm to be installed
-			},
-			automatic_installation = true,
-		})
+    require("mason-lspconfig").setup({
+      -- Install these LSPs automatically
+      ensure_installed = {
+        "bashls", -- requires npm to be installed
+        "lua_ls",
+        "jsonls", -- requires npm to be installed
+        "lemminx",
+        "marksman",
+        "yamlls", -- requires npm to be installed
+        "ruff_lsp", -- python linter and formatter
+        "pyright", -- python language server
+      },
+      automatic_installation = true,
+    })
 
-		require("mason-tool-installer").setup({
-			-- Install these linters, formatters, debuggers automatically
-			ensure_installed = {
-				"stylua", -- lua formatter
-				"debugpy",
-			},
-		})
+    require("mason-tool-installer").setup({
+      -- Install these linters, formatters, debuggers automatically
+      ensure_installed = {
+        "stylua", -- lua formatter
+        "debugpy",
+      },
+    })
 
-		vim.keymap.set(
-			"n",
-			"<leader>ma",
-			"<cmd>Mason<CR>",
-			{ noremap = true, silent = true, desc = "Open Mason Management" }
-		)
-	end,
+    vim.keymap.set(
+      "n",
+      "<leader>ma",
+      "<cmd>Mason<CR>",
+      { noremap = true, silent = true, desc = "Open Mason Management" }
+    )
+  end,
 }
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,110 +1,136 @@
 -- LSP Support
 return {
-	-- LSP Configuration
-	-- https://github.com/neovim/nvim-lspconfig
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		-- Useful status updates for LSP
-		-- https://github.com/j-hui/fidget.nvim
-		{ "j-hui/fidget.nvim", opts = {} },
+  -- LSP Configuration
+  -- https://github.com/neovim/nvim-lspconfig
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    -- Useful status updates for LSP
+    -- https://github.com/j-hui/fidget.nvim
+    { "j-hui/fidget.nvim",                   opts = {} },
 
-		-- Additional lua configuration, makes nvim stuff amazing!
-		-- https://github.com/folke/neodev.nvim
-		{ "folke/neodev.nvim" },
-		-- https://github.com/antosha417/nvim-lsp-file-operations
-		{ "antosha417/nvim-lsp-file-operations", config = true },
-		{ "stevanmilic/nvim-lspimport" },
-	},
-	config = function()
-		local lspconfig = require("lspconfig")
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    -- Additional lua configuration, makes nvim stuff amazing!
+    -- https://github.com/folke/neodev.nvim
+    { "folke/neodev.nvim" },
+    -- https://github.com/antosha417/nvim-lsp-file-operations
+    { "antosha417/nvim-lsp-file-operations", config = true },
+    { "stevanmilic/nvim-lspimport" },
+  },
+  config = function()
+    local lspconfig = require("lspconfig")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-		local keymap = vim.keymap -- for conciseness
-		local opts = { noremap = true, silent = true }
+    local keymap = vim.keymap -- for conciseness
+    local opts = { noremap = true, silent = true }
 
-		-- Global mappings
-		opts.desc = "Show buffer diagnostics"
-		keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+    -- Global mappings
+    opts.desc = "Show buffer diagnostics"
+    keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
-		opts.desc = "Show line diagnostics"
-		keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+    opts.desc = "Show line diagnostics"
+    keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-		opts.desc = "Go to previous diagnostic"
-		keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+    opts.desc = "Go to previous diagnostic"
+    keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
-		opts.desc = "Go to next diagnostic"
-		keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+    opts.desc = "Go to next diagnostic"
+    keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
-		local on_attach = function(client, bufnr)
-			opts.buffer = bufnr
+    local on_attach = function(client, bufnr)
+      opts.buffer = bufnr
 
-			-- LSP mappings
-			opts.desc = "Show LSP references"
-			keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+      -- LSP mappings
+      opts.desc = "Show LSP references"
+      keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
-			opts.desc = "Go to declaration"
-			keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+      opts.desc = "Go to declaration"
+      keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-			opts.desc = "Show LSP definitions"
-			keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+      opts.desc = "Show LSP definitions"
+      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
 
-			opts.desc = "Show LSP implementations"
-			keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+      opts.desc = "Show LSP implementations"
+      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
-			opts.desc = "Show LSP type definitions"
-			keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+      opts.desc = "Show LSP type definitions"
+      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-			opts.desc = "Signature help"
-			keymap.set("n", "<leader>gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts) -- signature help
+      opts.desc = "Signature help"
+      keymap.set("n", "<leader>gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts) -- signature help
 
-			opts.desc = "Document symbol"
-			keymap.set("n", "<leader>ds", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", opts) -- document symbol
+      opts.desc = "Document symbol"
+      keymap.set("n", "<leader>ds", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", opts) -- document symbol
 
-			opts.desc = "See available code actions"
-			keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+      opts.desc = "See available code actions"
+      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-			opts.desc = "Smart rename"
-			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+      opts.desc = "Smart rename"
+      keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-			opts.desc = "Show documentation for what is under cursor"
-			keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+      opts.desc = "Show documentation for what is under cursor"
+      keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-			opts.desc = "Restart LSP"
-			keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-		end
+      opts.desc = "Restart LSP"
+      keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+    end
 
-		-- used to enable autocompletion (assign to every lsp server config)
-		local capabilities = cmp_nvim_lsp.default_capabilities()
+    -- used to enable autocompletion (assign to every lsp server config)
+    local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Call setup on each LSP server
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({
-					on_attach = on_attach,
-					capabilities = capabilities,
-				})
-			end,
-		})
+    -- Call setup on each LSP server
+    require("mason-lspconfig").setup_handlers({
+      function(server_name)
+        lspconfig[server_name].setup({
+          on_attach = on_attach,
+          capabilities = capabilities,
+        })
+      end,
+    })
 
-		-- Lua LSP settings
-		lspconfig.lua_ls.setup({
-			settings = {
-				Lua = {
-					diagnostics = {
-						-- Get the language server to recognize the `vim` global
-						globals = { "vim" },
-					},
-				},
-			},
-		})
+    -- Lua LSP settings
+    lspconfig.lua_ls.setup({
+      settings = {
+        Lua = {
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = { "vim" },
+          },
+        },
+      },
+    })
 
-		-- Globally configure all LSP floating preview popups (like hover, signature help, etc)
-		local open_floating_preview = vim.lsp.util.open_floating_preview
-		function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-			opts = opts or {}
-			opts.border = opts.border or "rounded" -- Set border to rounded
-			return open_floating_preview(contents, syntax, opts, ...)
-		end
-	end,
+    -- Ruff LSP settings
+    require("lspconfig").ruff_lsp.setup({
+      on_attach = function(client, bufnr)
+        if client.name == "ruff_lsp" then
+          -- Disable hover in favor of Pyright
+          client.server_capabilities.hoverProvider = false
+        end
+      end,
+    })
+
+    -- Pyright LSP settings
+    require("lspconfig").pyright.setup({
+      settings = {
+        pyright = {
+          -- Using Ruff's import organizer
+          disableOrganizeImports = true,
+        },
+        python = {
+          analysis = {
+            -- Ignore all files for analysis to exclusively use Ruff for linting
+            ignore = { "*" },
+          },
+        },
+      },
+    })
+
+    -- Globally configure all LSP floating preview popups (like hover, signature help, etc)
+    local open_floating_preview = vim.lsp.util.open_floating_preview
+    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+      opts = opts or {}
+      opts.border = opts.border or "rounded" -- Set border to rounded
+      return open_floating_preview(contents, syntax, opts, ...)
+    end
+  end,
 }
