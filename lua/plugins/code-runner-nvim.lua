6e09a1d893b50560f6458b99cc5975e63bce842c
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-26 23:05:51 +0800

    format the code runner config and comment out the filetypes config

--- lua/plugins/code-runner-nvim.lua
+++ lua/plugins/code-runner-nvim.lua
@@ -1,22 +1,22 @@
 return {
-  -- https://github.com/CRAG666/code_runner.nvim
-  "CRAG666/code_runner.nvim",
-  opts = {
-    filetypes = {
-      -- I'm not sure why but it doesn't work with the filetype setting.
-      -- I try to set the PYTHONPATH before running the code, but it doesn't work.
-      python = {
-        "python3 -u",
-      },
-    },
-  },
-  keys = {
-    { "<leader>r",   ":RunCode<CR>",     { noremap = true, silent = false } },
-    { "<leader>rf",  ":RunFile<CR>",     { noremap = true, silent = false } },
-    { "<leader>rft", ":RunFile tab<CR>", { noremap = true, silent = false } },
-    { "<leader>rp",  ":RunProject<CR>",  { noremap = true, silent = false } },
-    { "<leader>rc",  ":RunClose<CR>",    { noremap = true, silent = false } },
-    -- { "<leader>crf", ":CRFiletype<CR>",  { noremap = true, silent = false } },
-    -- { "<leader>crp", ":CRProjects<CR>",  { noremap = true, silent = false } },
-  },
+	-- https://github.com/CRAG666/code_runner.nvim
+	"CRAG666/code_runner.nvim",
+	opts = {
+		filetypes = {
+			-- I'm not sure why but it doesn't work with the filetype setting.
+			-- I try to set the PYTHONPATH before running the code, but it doesn't work.
+			-- python = {
+			--   "python3 -u",
+			-- },
+		},
+	},
+	keys = {
+		{ "<leader>r", ":RunCode<CR>", { noremap = true, silent = false } },
+		{ "<leader>rf", ":RunFile<CR>", { noremap = true, silent = false } },
+		{ "<leader>rft", ":RunFile tab<CR>", { noremap = true, silent = false } },
+		{ "<leader>rp", ":RunProject<CR>", { noremap = true, silent = false } },
+		{ "<leader>rc", ":RunClose<CR>", { noremap = true, silent = false } },
+		{ "<leader>crf", ":CRFiletype<CR>", { noremap = true, silent = false } },
+		{ "<leader>crp", ":CRProjects<CR>", { noremap = true, silent = false } },
+	},
 }
