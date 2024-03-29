15b7786d1ed43697a904a0ca291784bd7aa95e0e
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-03-29 16:29:08 +0800

    format code for indent blankline

--- lua/plugins/indent-blankline-nvim.lua
+++ lua/plugins/indent-blankline-nvim.lua
@@ -2,9 +2,9 @@
 return {
   -- https://github.com/lukas-reineke/indent-blankline.nvim
   "lukas-reineke/indent-blankline.nvim",
-  event = 'VeryLazy',
+  event = "VeryLazy",
   main = "ibl",
-  config = function()
+  config = function(_, opts)
     local highlight = {
       "RainbowRed",
       "RainbowYellow",
@@ -15,7 +15,7 @@ return {
       "RainbowCyan",
     }
 
-    local hooks = require "ibl.hooks"
+    local hooks = require("ibl.hooks")
     -- create the highlight groups in the highlight setup hook, so they are reset
     -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
@@ -28,6 +28,6 @@ return {
       vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
     end)
 
-    require("ibl").setup { indent = { highlight = highlight } }
-  end
+    require("ibl").setup({ indent = { highlight = highlight } })
+  end,
 }
