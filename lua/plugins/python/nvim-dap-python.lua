7fe28a2d4ea79aa4dd84ddeb2deb27801ee7da08
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-04-12 01:58:42 +0800

    add a python dap option to execute at working dir

--- lua/plugins/python/nvim-dap-python.lua
+++ lua/plugins/python/nvim-dap-python.lua
@@ -1,17 +1,25 @@
 return {
-	-- https://github.com/mfussenegger/nvim-dap-python
-	"mfussenegger/nvim-dap-python",
-	ft = "python",
-	dependencies = {
-		-- https://github.com/mfussenegger/nvim-dap
-		"mfussenegger/nvim-dap",
-		"rcarriga/nvim-dap-ui",
-		"linux-cultist/venv-selector.nvim",
-	},
-	config = function()
+  -- https://github.com/mfussenegger/nvim-dap-python
+  "mfussenegger/nvim-dap-python",
+  ft = "python",
+  dependencies = {
+    -- https://github.com/mfussenegger/nvim-dap
+    "mfussenegger/nvim-dap",
+    "rcarriga/nvim-dap-ui",
+    "linux-cultist/venv-selector.nvim",
+  },
+  config = function()
     -- I use vertual environment, so everytime change the project that have to install
     -- the `debugpy` in the virtual environment.
-		local path = require("venv-selector").get_active_path()
-		require("dap-python").setup(path)
-	end,
+    local path = require("venv-selector").get_active_path()
+    require("dap-python").setup(path)
+    table.insert(require("dap").configurations.python, 1, {
+      type = "python",
+      request = "launch",
+      name = "Launch file(working directory)",
+      program = "${file}",
+      cwd = vim.fn.getcwd(),
+      -- ... more options, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
+    })
+  end,
 }
