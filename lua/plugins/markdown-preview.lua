61ac054fcee2b8706cb0d64d0fa9b375d80876fc
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-04-18 01:15:45 +0800

    format the markdown preview plugin

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,12 +1,12 @@
 return {
-  -- https://github.com/iamcco/markdown-preview.nvim
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  ft = { "markdown" },
-  keys = {
-    { "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", mode = "n", desc = "Toggle Markdown Preview" },
-  },
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
+	-- https://github.com/iamcco/markdown-preview.nvim
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	ft = { "markdown" },
+	keys = {
+		{ "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", mode = "n", desc = "Toggle Markdown Preview" },
+	},
+	build = function()
+		vim.fn["mkdp#util#install"]()
+	end,
 }
