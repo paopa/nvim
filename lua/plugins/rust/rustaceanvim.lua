31d409272762c9566c08a60154ff280cbcc43bc8
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-04-28 15:51:22 +0800

    enable auto load for rust dap

--- lua/plugins/rust/rustaceanvim.lua
+++ lua/plugins/rust/rustaceanvim.lua
@@ -1,6 +1,17 @@
 return {
-  -- https://github.com/mrcjkb/rustaceanvim
-  "mrcjkb/rustaceanvim",
-  version = "^4", -- Recommended
-  lazy = false,  -- This plugin is already lazy
+	-- https://github.com/mrcjkb/rustaceanvim
+	"mrcjkb/rustaceanvim",
+	version = "^4", -- Recommended
+	lazy = false, -- This plugin is already lazy
+	ft = "rust",
+	dependencies = {
+		"mfussenegger/nvim-dap",
+	},
+	config = function()
+		vim.g.rustaceanvim = {
+			dap = {
+				autoload_configurations = true,
+			},
+		}
+	end,
 }
