dabdb45bee30fd8b255040fbfdb0fc15799d8046
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-05-09 22:16:03 +0800

    make the lazygit with float direction

--- lua/plugins/toggleterm-nvim.lua
+++ lua/plugins/toggleterm-nvim.lua
@@ -1,34 +1,34 @@
 return {
-  -- https://github.com/akinsho/toggleterm.nvim
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  opts = {
-    direction = "tab",
-  },
-  keys = {
-    { "<leader>tt", ":ToggleTerm<CR>",            mode = "n", desc = "open terminal" },
-    { "<leader>t1", ":1ToggleTerm<CR>",            mode = "n", desc = "open terminal" },
-    { "<leader>t2", ":2ToggleTerm<CR>",            mode = "n", desc = "open terminal" },
-    -- I saw the answer to solve how to exit terminal in the neovim in the following link
-    -- https://www.reddit.com/r/neovim/comments/wk1vcc/how_to_exit_terminal_or_float_term/
-    { "<C-t>",      "<C-\\><C-n>", mode = "t", desc = "escape terminal mode" },
-    {
-      "<leader>lg",
-      "<cmd>lua _lazygit_toggle()<CR>",
-      mode = "n",
-      { noremap = true, silent = true },
-      desc = "lazygit",
-    },
-  },
-  config = function(_, opts)
-    require("toggleterm").setup(opts)
+	-- https://github.com/akinsho/toggleterm.nvim
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {
+		direction = "tab",
+	},
+	keys = {
+		{ "<leader>tt", ":ToggleTerm<CR>", mode = "n", desc = "open terminal" },
+		{ "<leader>t1", ":1ToggleTerm<CR>", mode = "n", desc = "open terminal" },
+		{ "<leader>t2", ":2ToggleTerm<CR>", mode = "n", desc = "open terminal" },
+		-- I saw the answer to solve how to exit terminal in the neovim in the following link
+		-- https://www.reddit.com/r/neovim/comments/wk1vcc/how_to_exit_terminal_or_float_term/
+		{ "<C-t>", "<C-\\><C-n>", mode = "t", desc = "escape terminal mode" },
+		{
+			"<leader>lg",
+			"<cmd>lua _lazygit_toggle()<CR>",
+			mode = "n",
+			{ noremap = true, silent = true },
+			desc = "lazygit",
+		},
+	},
+	config = function(_, opts)
+		require("toggleterm").setup(opts)
 
-    -- lazygit
-    local Terminal = require("toggleterm.terminal").Terminal
-    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+		-- lazygit
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
-    function _lazygit_toggle()
-      lazygit:toggle()
-    end
-  end,
+		function _lazygit_toggle()
+			lazygit:toggle(_, "float")
+		end
+	end,
 }
