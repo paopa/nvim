3175cf31f757b27c046c2f88d5b1884b3d9d74b7
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-05-17 22:49:29 +0800

    comment out fine cmdline

--- lua/plugins/fine-cmdline.lua
+++ lua/plugins/fine-cmdline.lua
@@ -1,10 +1,12 @@
 return {
-  -- https://github.com/vonheikemen/fine-cmdline.nvim
-  "VonHeikemen/fine-cmdline.nvim",
-  requires = {
-    { "MunifTanjim/nui.nvim" },
-  },
-  config = function()
-    vim.api.nvim_set_keymap("n", ":", "<cmd>FineCmdline<CR>", { noremap = true })
-  end,
+    -- https://github.com/vonheikemen/fine-cmdline.nvim
+    -- I think this plugin has a lot of potential. however, it doesn't have a lot of features yet.
+    -- I'm going to keep an eye on it. so, commented it out for now.
+    -- "VonHeikemen/fine-cmdline.nvim",
+    -- requires = {
+    -- 	{ "MunifTanjim/nui.nvim" },
+    -- },
+    -- config = function()
+    -- 	vim.api.nvim_set_keymap("n", "<CR>", "<cmd>FineCmdline<CR>", { noremap = true })
+    -- end,
 }
