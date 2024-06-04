a7f5e539314fe3c8922b9151ae92b8e1ee010b7a
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-06-04 22:28:44 +0800

    venv-selector: add a venv hook to setup PYTHONPATH

--- lua/plugins/python/venv-selector-nvim.lua
+++ lua/plugins/python/venv-selector-nvim.lua
@@ -1,6 +1,19 @@
 -- I used this plugin to manage the virtualenvs to control which python interpreter to use for a project.
 -- and using code runner plugin to run the code with the selected interpreter.
 
+-- see https://github.com/linux-cultist/venv-selector.nvim/tree/main?tab=readme-ov-file#hooks
+--- @param venv_path string A string containing the absolute path to selected virtualenv
+--- @param venv_python string A string containing the absolute path to python binary in selected venv
+local function python_path_env(venv_path, venv_python)
+	-- Set the PYTHONPATH to include the selected virtualenv's site-packages directory
+	local version = io.popen(venv_python .. " --version"):read("*a"):match("Python (%d+.%d+)")
+	local lib_path = venv_path .. "/lib/python" .. version .. "/site-packages"
+
+	-- Add the working directory to the PYTHONPATH as well
+	local project_path = vim.fn.getcwd()
+	vim.fn.setenv("PYTHONPATH", lib_path .. ":" .. project_path)
+end
+
 return {
 	-- https://github.com/linux-cultist/venv-selector.nvim/tree/regexp
 	"linux-cultist/venv-selector.nvim",
@@ -18,5 +31,11 @@ return {
 	end,
 	keys = {
 		{ ",v", "<cmd>VenvSelect<cr>" },
+	},
+	opts = {
+		name = { ".venv", "venv" },
+		dap_enabled = true,
+		-- auto_refresh = false
+		changed_venv_hooks = { python_path_env },
 	},
 }
