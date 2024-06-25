7e3a9cf357e171f03640b1ca7525e311773e6563
Pao-Sheng Wang <paooap.oappao@gmail.com>
2024-06-25 22:06:13 +0800

    chore: code format

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,26 +1,27 @@
 -- Code Tree Support / Syntax Highlighting
 return {
-	-- https://github.com/nvim-treesitter/nvim-treesitter
-	"nvim-treesitter/nvim-treesitter",
-	event = "VeryLazy",
-	dependencies = {
-		-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
-	build = ":TSUpdate",
-	opts = {
-		highlight = {
-			enable = true,
-		},
-		indent = { enable = true },
-		auto_install = true, -- automatically install syntax support when entering new file type buffer
-		ensure_installed = {
-			"lua",
-			"python",
-		},
-	},
-	config = function(_, opts)
-		-- treesitter loads the opts in the config moudle and not the main module
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+    -- https://github.com/nvim-treesitter/nvim-treesitter
+    "nvim-treesitter/nvim-treesitter",
+    event = "VeryLazy",
+    dependencies = {
+        -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    build = ":TSUpdate",
+    opts = {
+        highlight = {
+            enable = true,
+        },
+        indent = { enable = true },
+        auto_install = true, -- automatically install syntax support when entering new file type buffer
+        ensure_installed = {
+            "lua",
+            "python",
+            "just",
+        },
+    },
+    config = function(_, opts)
+        -- treesitter loads the opts in the config moudle and not the main module
+        require("nvim-treesitter.configs").setup(opts)
+    end,
 }
